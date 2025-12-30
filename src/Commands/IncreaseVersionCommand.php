adcb61dd9280a6c81722465770463e9a3813db6e
Michal Drobny <michal.drobny@me.com>
2025-12-30 01:56:08 +0100

    feature: separate increment commands deleted. Single command that runs through Laravel prompts for increment instead.

--- src/Commands/IncreaseVersionCommand.php
+++ src/Commands/IncreaseVersionCommand.php
@@ -5,10 +5,12 @@ namespace DrobnyDev\ApplicationVersioning\Commands;
 use DrobnyDev\ApplicationVersioning\ApplicationVersioning;
 use DrobnyDev\ApplicationVersioning\Exceptions\InvalidArgumentException;
 use Illuminate\Console\Command;
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\select;
 
 class IncreaseVersionCommand extends Command
 {
-    protected $signature = 'application-version:increase-version {--T|type= : major, minor or patch}';
+    protected $signature = 'application-version:increase';
 
     protected $description = 'Increase major, minor or patch version';
 
@@ -17,26 +19,24 @@ class IncreaseVersionCommand extends Command
      */
     public function handle(): void
     {
-        switch ($this->option('type')) {
-            case '=major':
-                (new ApplicationVersioning)->increaseMajor();
-                $this->message('Major');
-                break;
-            case '=minor':
-                (new ApplicationVersioning)->increaseMinor();
-                $this->message('Minor');
-                break;
-            case '=patch':
-                (new ApplicationVersioning)->increasePatch();
-                $this->message('Patch');
-                break;
-            default:
-                $this->info('Please specify version type: major, minor or patch.');
-        }
-    }
+        $versionType = select(
+            label: 'Which version do you want to increase?',
+            options: ['major', 'minor', 'patch'],
+            default: 'patch'
+        );
 
-    private function message(string $type): void
-    {
-        $this->info("$type version increased. Current version is now: ".ApplicationVersioning::getFormatedVersion());
+        switch ($versionType) {
+            case 'major':
+                (new ApplicationVersioning)->increaseMajor();
+                break;
+            case 'minor':
+                (new ApplicationVersioning)->increaseMinor();
+                break;
+            case 'patch':
+                (new ApplicationVersioning)->increasePatch();
+                break;
+        }
+
+        info(ucfirst($versionType).' version increased. Current version is now: '.ApplicationVersioning::getFormatedVersion());
     }
 }
